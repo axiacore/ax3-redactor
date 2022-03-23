0a50707e9895f839e64bc9bbc7b35927648a2907
Felipe Gonzalez <felipe.gonzalez@axiacore.com>
2022-03-22 19:53:11 -0500

    feat(s3): Added s3 storage

--- redactor/models.py
+++ redactor/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-from ax3_model_extras.storages import get_upload_path
+from ax3_model_extras.storages import get_upload_path, get_storage
 
 
 class RedactorFile(models.Model):
-    file = models.FileField(upload_to=get_upload_path)
+    file = models.FileField(
+        upload_to=get_upload_path,
+        storage=get_storage(),
+    )
 
     name = models.CharField(max_length=255)
 
