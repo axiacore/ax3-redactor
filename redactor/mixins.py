c9fa66f3ea26254c7758ebce920fe0ee8dad142b
Felipe Gonzalez <felipe.gonzalez@axiacore.com>
2022-04-07 09:09:11 -0500

    fix(mixin): Fixed AttributeError

--- redactor/mixins.py
+++ redactor/mixins.py
@@ -2,12 +2,12 @@ class RedactorMixin:
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
 
-        for field in self.redactor_fields:
+        for field in getattr(self, 'redactor_fields', []):
             form.base_fields[field].widget.attrs['enable-redactor'] = True
 
-        for field in self.basic_redactor_fields:
+        for field in getattr(self, 'basic_redactor_fields', []):
             form.base_fields[field].widget.attrs['enable-basic-redactor'] = True
-        
+
         return form
 
     class Media:
