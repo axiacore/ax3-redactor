f7cd7da4b9f598b944afd1bb5810d8bd8d9b35f3
Felipe Gonzalez <felipe.gonzalez@axiacore.com>
2022-06-01 12:15:02 -0500

    fix: Fixed S3Boto3Storage NotImplementedError

--- redactor/views.py
+++ redactor/views.py
@@ -1,14 +1,13 @@
 from io import BytesIO
 
+from ax3_model_extras.webp import generate_webp
+from django.conf import settings
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
-
-from ax3_model_extras.webp import generate_webp
-
 from PIL import Image, UnidentifiedImageError
 
 from .models import RedactorFile
@@ -30,23 +29,27 @@ class RedactorView(UserPassesTestMixin, View):
         count = 0
         images = {}
         for file in request.FILES.getlist('file[]'):
-            try:
-                output_image = Image.open(file)
-                img_format = output_image.format
-                if output_image.format == 'JPEG':
-                    output_image = output_image.convert('RGB')
-
-                bytes_io = BytesIO()
-                output_image.save(bytes_io, format=img_format, optimize=True)
-
-                obj = RedactorFile.objects.create(
-                    file=SimpleUploadedFile(file.name, bytes_io.getvalue()),
-                    name=file.name,
-                    is_image=True,
-                )
-                generate_webp(image_field=obj.file)
-            except UnidentifiedImageError:
+            if getattr(settings, 'AWS_ACCESS_KEY_ID', None):
                 obj = RedactorFile.objects.create(file=file, name=file.name, is_image=False)
+
+            else:
+                try:
+                    output_image = Image.open(file)
+                    img_format = output_image.format
+                    if output_image.format == 'JPEG':
+                        output_image = output_image.convert('RGB')
+
+                    bytes_io = BytesIO()
+                    output_image.save(bytes_io, format=img_format, optimize=True)
+
+                    obj = RedactorFile.objects.create(
+                        file=SimpleUploadedFile(file.name, bytes_io.getvalue()),
+                        name=file.name,
+                        is_image=True,
+                    )
+                    generate_webp(image_field=obj.file)
+                except UnidentifiedImageError:
+                    obj = RedactorFile.objects.create(file=file, name=file.name, is_image=False)
 
             images['file-{}'.format(count)] = {'url': obj.file.url, 'name': obj.name, 'id': obj.id}
             count += 1
